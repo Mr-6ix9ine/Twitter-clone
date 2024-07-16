5b26cebed5ec537d5896b059533efe27c9589af8
Mr-6ix9ine <akshayks056@gmail.com>
2024-07-16 12:54:41 +0530

    full

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
-import Sidebar from "./components/common/SideBar";
-import RightPanel from "./components/common/RightPanel";
-import { Route, Routes } from "react-router-dom";
-import NotificationPage from "./pages/notification/Notification";
+import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
-import { Toaster } from 'react-hot-toast';
+import Sidebar from "./components/common/SideBar"
+
+import RightPanel from "./components/common/RightPanel";
+
+import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
@@ -39,23 +42,19 @@ function App() {
 		);
 	}
 
-
-
-
-
-
 	return (
 		<div className='flex max-w-6xl mx-auto'>
-      <Sidebar/>
+			{/* Common component, bc it's not wrapped with Routes */}
+			{authUser && <Sidebar />}
 			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/notification' element={<NotificationPage />} />
-				<Route path='/profile/:username' element={<ProfilePage />} />
+				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
+				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
+				<Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to='/' />} />
+				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
+				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
 			</Routes>
-      <RightPanel/>
-	  <Toaster/>
+			{authUser && <RightPanel />}
+			<Toaster />
 		</div>
 	);
 }
